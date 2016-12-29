11b3c537e4150cc0aaa73c6b299866b92ee81970
Joshua Harms <joshua@nozzlegear.com>
2016-12-29 10:51:54 -0600

    Bugfix: make async intent function gel with alexa-app package

--- skills/index.ts
+++ skills/index.ts
@@ -8,10 +8,16 @@ export default async function configure(server: Express) {
     const skill = new Alexa.app("rasputin");
     const api = new Stages(STAGES_API_KEY);
 
-    skill.intent("summaryIntent", {  }, async function (request, response) {
-        const summary = await api.getSummary("subscribed");
+    skill.intent("summaryIntent", {}, function (request, response) {
+        (async function () {
+            const summary = await api.getSummary("subscribed");
 
-        response.say(`Stages has ${summary.totalActiveSubscribers} subscribers, for a total of $${summary.totalMonthlyValue.toFixed(2)} per month.`);
+            response.say(`Stages has ${summary.totalActiveSubscribers} subscribers, for a total of $${summary.totalMonthlyValue.toFixed(2)} per month.`);
+            response.send();
+        } ());
+
+        // alexa-app package requires async functions to return false.
+        return false;
     });
 
     skill.express(server, "/skills/");
