df28322837d442d2d8b99d1c9be6b03ba89b2bce
Joshua Harms <joshua@nozzlegear.com>
2016-12-30 09:09:14 -0600

    Bugfix

--- skills/kmsignalr.ts
+++ skills/kmsignalr.ts
@@ -7,14 +7,16 @@ export default async function configure(alexa) {
 
     alexa.intent("kmsignalrIntent", {}, function (request, response) {
         (async function () {
-            const { Result } = await api.getSummary();
-            const onHold = Result.Data.find(d => d.Label === "On Hold");
-            const unshipped = Result.Data.find(d => d.Label === "Unshipped");
+            const apiResponse = await api.getSummary();
+            const result = apiResponse.Result;
+            const data = result.Data;
+            const onHold = data.find(d => d.Label === "Held");
+            const unshipped = data.find(d => d.Label === "Portraits Unshipped");
 
             if (unshipped.Count === 0) {
-                response.say(`Merricks had a total of ${Result.Total} portraits today, with ${onHold.Count} on hold for tomorrow.`);
+                response.say(`Merricks had a total of ${result.Total} portraits today, with ${onHold.Count} on hold for tomorrow.`);
             } else {
-                response.say(`Merricks currently has a total of ${Result.Total} portraits, with ${unshipped.Count} left to ship.`);
+                response.say(`Merricks currently has a total of ${result.Total} portraits, with ${unshipped.Count} left to ship.`);
             }
 
             response.send();
