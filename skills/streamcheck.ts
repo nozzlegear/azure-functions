7eea4efbe7491d328b3dac6b1fbd5fa0dad1bc3d
Joshua Harms <joshua@nozzlegear.com>
2017-02-22 20:11:34 -0600

    Added proper streamcheck response

--- skills/streamcheck.ts
+++ skills/streamcheck.ts
@@ -1,41 +1,40 @@
 import inspect from "logspect";
 import { Twitch } from "../modules/api";
+import * as Db from "../modules/database";
 import * as Constants from "../modules/constants";
 
 export default async function configure(alexa) {
     alexa.intent("summaryIntent", {}, function (request, response) {
-        inspect("Alexa request received", request);
-
         (async function () {
-            response.say("Hello world! This is the Streamcheck intent.");
+            // The accessToken is the CouchDB doc's id, use it to grab the user's twitch token.
+            const accessToken = request.sessionDetails.accessToken;
+            const user = await Db.TwitchAuthDb.get(accessToken);
 
             // Refresh the API with the user's access token
-            // const api = new Twitch(Constants.TWITCH_CLIENT_ID, Constants.TWITCH_CLIENT_SECRET, token);
+            const api = new Twitch(Constants.TWITCH_CLIENT_ID, Constants.TWITCH_CLIENT_SECRET, user.twitch_token);
 
-            // try {
-            //     const streams = await api.listFollowerStreams({});
-            //     const response: string[] = [];
+            try {
+                const streams = await api.listFollowerStreams({});
+                const responses: string[] = [];
 
-            //     if (streams._total === 0) {
-            //         response.push(`None of the channels you follow are streaming right now.`);
-            //     } else {
-            //         if (streams._total > 1) {
-            //             response.push(`${streams._total} streamers you follow are streaming right now.`);
-            //         }
+                if (streams._total === 0) {
+                    responses.push(`None of the channels you follow are streaming right now.`);
+                } else {
+                    if (streams._total > 1) {
+                        responses.push(`${streams._total} streamers you follow are streaming right now.`);
+                    }
 
-            //         streams.streams.forEach(stream => {
-            //             response.push(`${stream.channel.display_name} is streaming ${stream.channel.status}`);
-            //         })
-            //     }
+                    streams.streams.forEach(stream => {
+                        responses.push(`${stream.channel.display_name} is streaming ${stream.channel.status}`);
+                    })
+                }
 
-            //     res.json({ response: response.join(". ") });
-            // } catch (_e) {
-            //     const e: ApiError = _e;
+                response.say(responses.join(". "));
+            } catch (_e) {
+                inspect("Error retrieving followed streams", _e);
 
-            //     inspect("Error retrieving followed streams", e);
-
-            //     return next(e);
-            // }
+                response.say(`There was an error retrieving your followed streams. Sorry about that.`);
+            }
 
             response.send();
         } ());
