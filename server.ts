efd0d1d952a71c1fca97670bfe2dc2e88e6fa946
Joshua Harms <joshua@nozzlegear.com>
2017-04-18 16:37:15 -0500

    Moving lex from express to nginx proxy.

--- server.ts
+++ server.ts
@@ -65,30 +65,27 @@ async function startServer(hostname: string, port: number, securePort: number) {
         return next();
     } as any);
 
-    // Prepare encryption
-    const lexTempDir = path.join(os.tmpdir(), "acme-challenges");
-    const lex = letsEncrypt.create({
-        // You must set server to https://acme-v01.api.letsencrypt.org/directory after you have tested that your setup works.
-        server: (!ISLIVE) ? "staging" : "https://acme-v01.api.letsencrypt.org/directory",
-        challenges: { 'tls-sni-01': require('le-challenge-sni').create({ webrootPath: lexTempDir }) },
-        challengeType: 'tls-sni-01',
-        store: require('le-store-certbot').create({ webrootPath: lexTempDir }),
-        approveDomains: !ISLIVE ? ["127.0.0.1"] : ["alexa.nozzlegear.com"],
-        email: `joshua@nozzlegear.com`,
-        agreeTos: true,
-    });
+    // // Prepare encryption
+    // const lexTempDir = path.join(os.tmpdir(), "acme-challenges");
+    // const lex = letsEncrypt.create({
+    //     // You must set server to https://acme-v01.api.letsencrypt.org/directory after you have tested that your setup works.
+    //     server: (!ISLIVE) ? "staging" : "https://acme-v01.api.letsencrypt.org/directory",
+    //     challenges: { 'tls-sni-01': require('le-challenge-sni').create({ webrootPath: lexTempDir }) },
+    //     challengeType: 'tls-sni-01',
+    //     store: require('le-store-certbot').create({ webrootPath: lexTempDir }),
+    //     approveDomains: !ISLIVE ? ["127.0.0.1"] : ["alexa.nozzlegear.com"],
+    //     email: `joshua@nozzlegear.com`,
+    //     agreeTos: true,
+    // });
 
-    return {
-        http: http.createServer(ISLIVE ? httpsRedirect() : app).listen(port, hostname),
-        https: https.createServer(lex.httpsOptions, lex.middleware(app)).listen(securePort, hostname),
-    };
+    return http.createServer(app).listen(port, hostname);
 }
 
 const host = process.env.HOST || "127.0.0.1";
 const port = process.env.PORT || 3000;
 const securePort = process.env.SECURE_PORT || 3001;
 
-startServer(host, port, securePort).then((servers: { http: http.Server, https: https.Server }) => {
+startServer(host, port, securePort).then((server: https.Server | http.Server) => {
     inspect(`HTTP and HTTPS servers are listening on ${host}:${port} and ${host}:${securePort}.`);
 }).catch(e => {
     inspect("Error starting server.", e);
