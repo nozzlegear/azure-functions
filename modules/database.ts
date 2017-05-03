603006be88d49ff232169e788d983d1ced06617a
Joshua Harms <joshua@nozzlegear.com>
2017-05-03 13:53:19 -0500

    Bugfix: Davenport clients should also use auth options.

--- modules/database.ts
+++ modules/database.ts
@@ -4,14 +4,13 @@ import { Client, configureDatabase, ClientOptions } from "davenport";
 
 const dbName = "alexa_twitch_oauth";
 const accountLinkDbName = "alexa_account_link_requests";
+const options: ClientOptions = {
+    warnings: false,
+    username: Constants.COUCHDB_USERNAME,
+    password: Constants.COUCHDB_PASSWORD,
+};
 
 export default async function configureAll() {
-    const options: ClientOptions = {
-        warnings: false,
-        username: Constants.COUCHDB_USERNAME,
-        password: Constants.COUCHDB_PASSWORD,
-    };
-
     await configureDatabase(Constants.COUCHDB_URL, {
         name: dbName,
     }, options);
@@ -21,5 +20,5 @@ export default async function configureAll() {
     }, options);
 }
 
-export const AccountLinkDb = new Client<AccountLinkRequest>(Constants.COUCHDB_URL, accountLinkDbName, { warnings: false });
-export const TwitchAuthDb = new Client<TwitchAuth>(Constants.COUCHDB_URL, dbName, { warnings: false });
+export const AccountLinkDb = new Client<AccountLinkRequest>(Constants.COUCHDB_URL, accountLinkDbName, options);
+export const TwitchAuthDb = new Client<TwitchAuth>(Constants.COUCHDB_URL, dbName, options);
