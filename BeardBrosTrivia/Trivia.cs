39052a9dd64b9014c59ac41e1ea2bb01e236b5a2
Joshua Harms <joshua@nozzlegear.com>
2017-07-19 14:50:29 -0500

    Converting BeardBrosTrivia to a C# app for (hopefully) faster startup time.

---
+++ BeardBrosTrivia/Trivia.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.ApplicationInsights;
+using Microsoft.ApplicationInsights.Extensibility;
+using Microsoft.Azure.WebJobs;
+using Microsoft.ProjectOxford.Vision;
+using Newtonsoft.Json.Linq;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using System.Net;
+
+namespace BeardBrosTrivia
+{
+    public class TriviaBoi
+    {
+        [FunctionName("BeardBrosTrivia")]
+        public static HttpResponseMessage Run([HttpTrigger(AuthorizationLevel.Anonymous, "GET", WebHookType = "genericJson")] HttpRequestMessage req)
+        {
+            return req.CreateResponse("Hello nerd");
+        }
+    }
+}
