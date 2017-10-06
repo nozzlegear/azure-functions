9342e9b130bab8d808dc2b8626b29896d4d54e02
Joshua Harms <joshua@nozzlegear.com>
2017-10-06 09:56:50 -0500

    Ignore paket's packages folder.

--- deploy.sh
+++ deploy.sh
@@ -8,7 +8,7 @@ apiUrl="https://nozzlegear-alexa.scm.azurewebsites.net/api/zip/site/wwwroot"
 # Seem to be running into some kind of cache issue that doesn't overwrite the previous deployment.zip, so we'll delete that.
 rm $fileName
 
-zip -r $fileName . -x node_modules/\* \*.zip .git/\* */obj/\* >/dev/null
+zip -r $fileName . -x node_modules/\* packages/\* \*.zip .git/\* */obj/\* >/dev/null
 echo "Zipped files, uploading to Azure..."
 curl -X PUT --data-binary "@$fileName" --user "$AZURE_FUNC_USERNAME:$AZURE_FUNC_PASSWORD" $apiUrl
 
