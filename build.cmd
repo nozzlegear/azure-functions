fca4307e26417ebe919bc8f43a24393c08eea92d
Joshua Harms <joshua@nozzlegear.com>
2017-10-08 00:04:37 -0500

    Do not copy node_modules file
    All node functions are being webpacked, so skipping the copy will improve deployment performance

--- build.cmd
+++ build.cmd
@@ -74,7 +74,7 @@ yarn build
 
 :: 3. KuduSync
 IF /I "%IN_PLACE_DEPLOYMENT%" NEQ "1" (
-  call :ExecuteCmd "%KUDU_SYNC_CMD%" -v 50 -f ./ -t "%DEPLOYMENT_TARGET%" -n "%NEXT_MANIFEST_PATH%" -p "%PREVIOUS_MANIFEST_PATH%" -i ".git;.hg;.deployment;deploy.cmd"
+  call :ExecuteCmd "%KUDU_SYNC_CMD%" -v 50 -f ./ -t "%DEPLOYMENT_TARGET%" -n "%NEXT_MANIFEST_PATH%" -p "%PREVIOUS_MANIFEST_PATH%" -i ".git;.hg;.deployment;deploy.cmd;node_modules;"
   IF !ERRORLEVEL! NEQ 0 goto error
 )
 
