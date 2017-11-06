c6f4248aa2760d03caf271efda444d575029187b
Joshua Harms <joshua@nozzlegear.com>
2017-11-05 22:50:36 -0600

    Pull CC list from json env variable

--- sci-tally-tool/Library.fs
+++ sci-tally-tool/Library.fs
@@ -1,12 +1,9 @@
 module SciTallyTool
 
     open System
-    open System.Net
-    open System.Threading
     open System.Collections.Generic
     open Microsoft.Azure.WebJobs
     open Microsoft.Azure.WebJobs.Host
-    open Microsoft.Azure.WebJobs.Extensions
     open Newtonsoft.Json
     open HttpFs.Client
     open Hopac
@@ -46,7 +43,7 @@ module SciTallyTool
         test: bool
     }
 
-    let envVarRequired key = 
+    let envVarRequired key =
         let value = System.Environment.GetEnvironmentVariable key
 
         if isNull value then failwithf "Environment Variable \"%s\" was not found." key
@@ -56,26 +53,26 @@ module SciTallyTool
     let envVarDefault key defaultValue =
         let value = System.Environment.GetEnvironmentVariable key
 
-        match value with 
+        match value with
         | s when isNull s || s = "" -> defaultValue
         | s -> s
 
-    let ifOptionIsSome (opt: 'optionType option) (ifOptionIsSomeFunc: 'optionType -> 'returnType -> 'returnType) (ifOptionIsNoneValue: 'returnType): 'returnType = 
-        match opt with 
+    let ifOptionIsSome (opt: 'optionType option) (ifOptionIsSomeFunc: 'optionType -> 'returnType -> 'returnType) (ifOptionIsNoneValue: 'returnType): 'returnType =
+        match opt with
         | Some o -> ifOptionIsSomeFunc o ifOptionIsNoneValue
         | None -> ifOptionIsNoneValue
 
-    let prepareRequest method url authHeader body = 
-        let message = 
+    let prepareRequest method url authHeader body =
+        let message =
             Request.createUrl method url
-            |> ifOptionIsSome body (fun body message -> 
+            |> ifOptionIsSome body (fun body message ->
                 let serializedBody = JsonConvert.SerializeObject body
-                let contentTypeHeader = 
-                    ContentType.parse "application/json" 
-                    |> Option.get 
+                let contentTypeHeader =
+                    ContentType.parse "application/json"
+                    |> Option.get
                     |> RequestHeader.ContentType
 
-                message 
+                message
                 |> Request.bodyString serializedBody
                 |> Request.setHeader contentTypeHeader)
             |> ifOptionIsSome authHeader (fun header message -> message |> Request.setHeader header)
@@ -101,46 +98,34 @@ module SciTallyTool
         let swuTemplateId = envVarRequired "SCI_TALLY_SWU_TEMPLATE_ID"
         let formatEmail name = sprintf "%s@%s" name emailDomain
 
-        let emailRecipient: SwuRecipient = 
+        let emailRecipient: SwuRecipient =
             if isLive then { name = "Mike"; address = formatEmail "mikef" }
             else { name = "Joshua Harms"; address = formatEmail "josh" }
         let emailCcs: SwuRecipient list =
-            if isLive then 
-                [
-                    {
-                        name = "Tim"
-                        address = formatEmail "tim"
-                    }
-                    {
-                        name = "Joshua Harms"
-                        address = formatEmail "josh"
-                    }
-                    {
-                        name = "Leta"
-                        address = formatEmail "letaf"
-                    }
-                ]
+            if isLive then
+                let listJson = envVarRequired "SCI_TALLY_CC_LIST"
+                JsonConvert.DeserializeObject<SwuRecipient list>(listJson)
             else []
-        let sender = 
+        let sender =
             {
                 name = "KMSignalR Superintendent"
                 address = formatEmail "superintendent"
-                replyTo = formatEmail "superintendent" 
-            }    
+                replyTo = formatEmail "superintendent"
+            }
 
-        let base64HeaderValue = 
-            sprintf "%s:" swuKey 
-            |> Text.Encoding.UTF8.GetBytes 
+        let base64HeaderValue =
+            sprintf "%s:" swuKey
+            |> Text.Encoding.UTF8.GetBytes
             |> Convert.ToBase64String
-        let header = Custom ("Authorization", sprintf "Basic %s" base64HeaderValue) 
+        let header = Custom ("Authorization", sprintf "Basic %s" base64HeaderValue)
         let url = "https://api.sendwithus.com/api/v1/send"
-        let message = 
+        let message =
             {
                 template = swuTemplateId
                 recipient = emailRecipient
                 cc = emailCcs
                 sender = sender
-                template_data = 
+                template_data =
                     {
                         date = tallyStartDate.ToString ("MMM dd, yyyy")
                         tally = tally
@@ -149,12 +134,12 @@ module SciTallyTool
         let! response =
             prepareRequest HttpMethod.Post url (Some header) (Some message)
             |> sendRequest
-        
+
         return response |> JsonConvert.DeserializeObject<SwuResponse>
     }
 
     let public Run(myTimer: TimerInfo, log: TraceWriter) =
-        
+
         sprintf "SCI Tally Tool executing at: %s" (DateTime.Now.ToString())
         |> log.Info
 
@@ -166,10 +151,10 @@ module SciTallyTool
         let endDate = midnight ()
         let startDate = endDate.AddDays -7.
         let protocol = if String.contains "localhost" apiDomain then "http" else "https"
-        let url = 
-            sprintf "%s://%s/api/v1/orders/tally/sources?since=%i&until=%i" 
-                protocol 
-                apiDomain 
+        let url =
+            sprintf "%s://%s/api/v1/orders/tally/sources?since=%i&until=%i"
+                protocol
+                apiDomain
                 (startDate |> toJsTimestamp)
                 (endDate |> toJsTimestamp)
 
@@ -181,22 +166,22 @@ module SciTallyTool
             |> JsonConvert.DeserializeObject<Tally>
 
         match summaryResponse.Count with
-        | 0 -> 
-            printfn "Tally response contained an empty summary. Was the `since` parameter (%i) incorrect?" 
+        | 0 ->
+            printfn "Tally response contained an empty summary. Was the `since` parameter (%i) incorrect?"
                 (startDate |> toJsTimestamp)
-        | _ -> 
+        | _ ->
             summaryResponse
             |> Seq.iter (fun kvp -> printfn "%s: %i portraits" kvp.Key kvp.Value)
 
         let emailResponse =
-            try 
+            try
                 summaryResponse
                 |> Seq.map (fun kvp -> { source = kvp.Key; count = kvp.Value })
                 |> List.ofSeq
                 |> sendEmailMessage startDate
                 |> run
-            with 
-            | e -> 
+            with
+            | e ->
                 log.Error("API call to SendWithUs failed.", e)
                 reraise ()
 
